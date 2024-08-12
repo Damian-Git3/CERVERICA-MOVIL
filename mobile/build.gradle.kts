10d63e70600b3afdabe2c4496a0064cbb19257df
IDGS-802-21000151 <jesusluviancontacto@gmail.com>
2024-08-12 12:01:42 -0600

    Nuevas pantallas

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.cerverica"
-        minSdk = 30
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
