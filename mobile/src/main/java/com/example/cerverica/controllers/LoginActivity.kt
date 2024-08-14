35e8c2ce5454c52713842ef4e9ce6fa461d63e31
IDGS-802-21000151 <jesusluviancontacto@gmail.com>
2024-08-14 13:34:10 -0600

    Avance

--- mobile/src/main/java/com/example/cerverica/controllers/LoginActivity.kt
+++ mobile/src/main/java/com/example/cerverica/controllers/LoginActivity.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +19,7 @@ import com.example.cerverica.controllers.AdminActivity
 import com.example.cerverica.controllers.cliente.ClienteActivity
 import com.example.cerverica.controllers.EmpleadoActivity
 import com.example.cerverica.controllers.RegistroActivity
+import com.google.android.material.progressindicator.LinearProgressIndicator
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -31,6 +34,7 @@ class LoginActivity : AppCompatActivity() {
         val passwordEditText = findViewById<EditText>(R.id.password)
         val loginButton = findViewById<Button>(R.id.login_button)
         val registerButton = findViewById<TextView>(R.id.register_button)
+        val progressBar = findViewById<LinearProgressIndicator>(R.id.progressBar)
 
         val backButton = findViewById<ImageView>(R.id.back_button)
         backButton.setOnClickListener {
@@ -42,44 +46,93 @@ class LoginActivity : AppCompatActivity() {
             val password = passwordEditText.text.toString()
 
             if (email.isBlank() || password.isBlank()) {
-                Toast.makeText(this, "Por favor ingrese su correo y contraseña", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Por favor ingrese su correo y contraseña", Toast.LENGTH_SHORT)
+                    .show()
                 return@setOnClickListener
             }
 
+            progressBar.setVisibility(View.VISIBLE);
+
             val loginRequest = LoginRequest(email, password)
 
-            RetrofitClient.instance.postLogin(loginRequest).enqueue(object : Callback<LoginResponse> {
-                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-                    Log.d("API_RESPONSE", "Response: ${response}")
-                    if (response.isSuccessful) {
-                        val loginResponse = response.body()
-                        if (loginResponse?.isSuccess == true) {
-                            // Guardar el token en SharedPreferences
-                            saveToken(loginResponse.token)
+            RetrofitClient.instance.postLogin(loginRequest)
+                .enqueue(object : Callback<LoginResponse> {
+                    override fun onResponse(
+                        call: Call<LoginResponse>,
+                        response: Response<LoginResponse>
+                    ) {
+                        Log.d("API_RESPONSE", "Response: ${response}")
+                        if (response.isSuccessful) {
+                            val loginResponse = response.body()
+                            if (loginResponse?.isSuccess == true) {
+                                // Guardar el token en SharedPreferences
+                                saveToken(loginResponse.token)
 
-                            // Guardar cualquier otra información del usuario según sea necesario
-                            saveUserInfo(loginResponse.idUsuario, loginResponse.nombre, loginResponse.role)
+                                // Guardar cualquier otra información del usuario según sea necesario
+                                saveUserInfo(
+                                    loginResponse.idUsuario,
+                                    loginResponse.nombre,
+                                    loginResponse.role
+                                )
 
-                            // Redirigir según el rol del usuario
-                            println(loginResponse.role)
-                            when (loginResponse.role) {  // Suponiendo que "roles" es una lista y tomas el primer rol
-                                "Admin" -> startActivity(Intent(this@LoginActivity, AdminActivity::class.java))
-                                "Produccion" -> startActivity(Intent(this@LoginActivity, EmpleadoActivity::class.java))
-                                "Cliente" -> startActivity(Intent(this@LoginActivity, ClienteActivity::class.java))
-                                else -> Toast.makeText(this@LoginActivity, "Rol desconocido", Toast.LENGTH_SHORT).show()
+                                // Redirigir según el rol del usuario
+                                println(loginResponse.role)
+                                when (loginResponse.role) {  // Suponiendo que "roles" es una lista y tomas el primer rol
+                                    "Admin" -> startActivity(
+                                        Intent(
+                                            this@LoginActivity,
+                                            AdminActivity::class.java
+                                        )
+                                    )
+
+                                    "Produccion" -> startActivity(
+                                        Intent(
+                                            this@LoginActivity,
+                                            EmpleadoActivity::class.java
+                                        )
+                                    )
+
+                                    "Cliente" -> startActivity(
+                                        Intent(
+                                            this@LoginActivity,
+                                            ClienteActivity::class.java
+                                        )
+                                    )
+
+                                    else -> Toast.makeText(
+                                        this@LoginActivity,
+                                        "Rol desconocido",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            } else {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    loginResponse?.message,
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
                         } else {
-                            Toast.makeText(this@LoginActivity, loginResponse?.message, Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "No successful: ${response.message()}",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
-                    } else {
-                        Toast.makeText(this@LoginActivity, "No successful: ${response.message()}", Toast.LENGTH_SHORT).show()
-                    }
-                }
 
-                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                    Toast.makeText(this@LoginActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
-                }
-            })
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                    override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Error: ${t.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        progressBar.setVisibility(View.GONE);
+                    }
+                })
         }
 
         registerButton.setOnClickListener {
@@ -89,7 +142,8 @@ class LoginActivity : AppCompatActivity() {
 
     // Función para guardar el token en SharedPreferences
     private fun saveToken(token: String) {
-        val sharedPref = MyApplication.context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+        val sharedPref =
+            MyApplication.context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
         val editor = sharedPref.edit()
         editor.putString("token", token)
         editor.apply()
@@ -97,7 +151,8 @@ class LoginActivity : AppCompatActivity() {
 
     // Función para guardar información adicional del usuario en SharedPreferences
     private fun saveUserInfo(idUsuario: String?, nombre: String?, role: String) {
-        val sharedPref = MyApplication.context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+        val sharedPref =
+            MyApplication.context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
         val editor = sharedPref.edit()
         editor.putString("idUsuario", idUsuario)
         editor.putString("nombre", nombre)
