2a6b5a19857c1c3503611c321f72409c8f057b65
IDGS-802-21000151 <jesusluviancontacto@gmail.com>
2024-08-15 01:57:23 -0600

    Cambios clase

--- mobile/src/main/java/com/example/cerverica/apiservice/RetrofitClient.kt
+++ mobile/src/main/java/com/example/cerverica/apiservice/RetrofitClient.kt
@@ -10,8 +10,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import com.example.cerverica.MyApplication
 
 object RetrofitClient {
-    private const val BASE_DOMAIN = "http://192.168.137.1:5000"
-    //private const val BASE_DOMAIN = "http://10.0.2.2:5000"
+    private const val BASE_DOMAIN = "http://192.168.1.8:5000"
     private const val BASE_URL = "${BASE_DOMAIN}/api/"
 
     private fun getToken(): String? {
